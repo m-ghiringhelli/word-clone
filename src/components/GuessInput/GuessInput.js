4df4ca39d7e2f8db2b038cb8c805f9382ad6bcf9
m-ghiringhelli <mghiring@gmail.com>
2023-02-10 11:30:56 -0800

    update GuessInput to be controlled / submit a guess object

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,28 @@
 import React from 'react'
 
 export default function GuessInput() {
+  const [guess, setGuess] = React.useState({});
+  const [guessText, setGuessText] = React.useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setGuess({ guess: guessText });
+    setGuessText('');
+  }
+
+  function handleChange(e) {
+    if (guessText.length >= 5) return;
+    setGuessText(e.target.value.toUpperCase());
+  }
+
   return (
-    <div>GuessInput</div>
+    <form className='guess-input-wrapper' onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter Guess:</label>
+      <input 
+        id="guess-input" 
+        type="text" 
+        value={guessText}
+        onChange={handleChange}></input>
+    </form>
   )
 }
